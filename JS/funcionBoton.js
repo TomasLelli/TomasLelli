30628b200d6752d4dbf27e51d95b9794ef2ebea5
TomasLelli <tomilelli111@gmail.com>
2022-08-24 16:36:25 -0300

    muchas modificaciones y uso de scss

---
+++ JS/funcionBoton.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".barra");
+const navMenu = document.querySelector(".navegation ul")
+
+navToggle.addEventListener("click", ()=>{
+    navMenu.classList.toggle("show")
+})
